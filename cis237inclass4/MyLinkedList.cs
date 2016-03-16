25060b2664f3bb73e6e4fa102b111c7367e4d723
Audre Staffen <audrewow@gmail.com>
2016-03-16 19:06:06 -0400

    Add retrieve and delete methods to myLinkedList.

--- cis237inclass4/MyLinkedList.cs
+++ cis237inclass4/MyLinkedList.cs
@@ -71,5 +71,133 @@ namespace cis237inclass4
             // done for both of them. 
             Tail = node;
         }
+
+        public string Retrieve(int position)
+        {
+            // Used as a temporary pointer to a spot within the linked list. 
+            Node tempNode = Head;
+
+            // Used to hold the node at the index indicated by the passed in position.
+            Node returnNode = null;
+
+            // Counter to see where we are in the list. 
+            int count = 0;
+
+            // While our teempNode is not at the end of the list.
+            while (tempNode != null)
+            {
+                // If the count and the position match. This means that it will be 
+                // zero based. If we wanted it t be 1 based, we would need to subtract
+                // 1 from the position.
+                if (count == position)
+                {
+                    // Set the returnNode var to the tempNode, which is the one we were looking for
+                    returnNode = tempNode;
+                }
+
+                // Increment the count so that we actually move through the list.
+                count++;
+                
+                // Set the tempNode to tempNode's next property, which will move us to the next
+                // node in the linked list. 
+                tempNode = tempNode.Next;
+            }
+
+            // We are going to use a ternary operator to do a smaller version of an if. 
+            // This could easily be written as an if/else. Essentially the part in the ()
+            // is the test, and the part between the ? and the : is what will happen if tru.
+            // The part after the : is what will happen when false. 
+            return (returnNode != null) ? returnNode.Data : null;
+        }
+
+        public bool Delete(int position)
+        {
+            // return value for the method
+            bool returnBool = false;
+
+            // Set current to Head.
+            Current = Head;
+
+            // If the position that we want to remove is the very first node, we need to do things
+            // different than if it is 1 through the end. 
+            // This part is equivalent to always removing from the front. (hint hint)
+            if (position == 0)
+            {
+                // Set the Head to the current.Next, which will make Head point to the node
+                // at index 1, instead of index 0.
+                Head = Current.Next;
+
+                // Set the Next property of Current to null so that the current 
+                // (which was the old Head) does not point to any other node. 
+                // This line is probably not "required," but it does illustrate how
+                // to clean up a node so it no longer points to anything. 
+                Current.Next = null;
+
+                // Set the current (was was the old Head) to null. Now that node no longer
+                // exists and can be garbaged collected. 
+                Current = null;
+
+                // Check to see if the Head is null. If so, the Tail must become null as well
+                // because it means we just deleted the last node in the list.
+                if (Head == null)
+                {
+                    Tail = null;
+                }
+
+                // Set the return bool to true since the remove was successful
+                returnBool = true;
+            }
+
+            else
+            {
+                // Set a tempNode to the first position in the linked list. 
+                Node tempNode = Head;
+
+                // Declare a previous temp that will get a value after the tempNode moves
+                Node previousTempNode = null;
+
+                // Start a counter to use to move through the linked list. 
+                int count = 0;
+
+                // Loop until the tempNode is null, which means we are at the end.
+                while (tempNode != null)
+                {
+                    // If we match the position and the count we are on, we found
+                    // the one that we need to delete.
+                    if (count == position)
+                    {
+                        // Set the previous node's next to the temp node's next
+                        // jumping over the tempNode. The previous node's next will now
+                        // point to the node AFTER the tempNode. 
+                        previousTempNode.Next = tempNode.Next;
+
+                        // Check to see if we are deleting the very last node in the list.
+                        // If so, we need to move the Tail pointer. 
+                        if (tempNode.Next == null)
+                        {
+                            // Set Tail to previousTempNode, which is the new end of the list. 
+                            Tail = previousTempNode;
+                        }
+
+                        // We found the one to delete, so set the return bool to true.
+                        returnBool = true;
+                    }
+
+                    // Increment the count so we move through the linked list.
+                    count++;
+
+                    // Set the previousTempNode to the current tempNode. This way when we
+                    // move the tempNode forward, we will still have a pointer to where
+                    // the tempNode was before it moved. 
+                    previousTempNode = tempNode;
+
+                    // Set the tempNode to tempNode's next property, which will move it down
+                    // the linked list one position. 
+                    tempNode = tempNode.Next;
+                }
+            }
+
+            return returnBool;
+        }
     }
 }
--- cis237inclass4/Program.cs
+++ cis237inclass4/Program.cs
@@ -27,6 +27,34 @@ namespace cis237inclass4
             {
                 Console.WriteLine(x.Data);
             }
+
+            // Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Use the retrieve method to start at the Head of the linked list,
+            // walk through it, and return the value that is at the passed in index.
+            Console.WriteLine(myLinkedList.Retrieve(2));
+            Console.WriteLine(myLinkedList.Retrieve(1));
+
+            // Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Do some deletes of the linked lists.
+            // This will delete the first one in the list.
+            myLinkedList.Delete(0);
+            // Now there are 3, and this will delete the middle one.
+            myLinkedList.Delete(1);
+            // Now ther eare 2, and this will delete the last one.
+            myLinkedList.Delete(1);
+
+            // Now there is only 1, and this loop will print it out. 
+            for (Node x = myLinkedList.Head; x != null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
+
         }
     }
 }
